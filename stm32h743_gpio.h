a7f42f87107294679ee3fbda3f3bf2ac1a0afbae
Yurevich Pavel <upv@bk.ru>
2021-04-27 10:02:50 +0200

    Update stm32h743_gpio.h

--- stm32h743_gpio.h
+++ stm32h743_gpio.h
@@ -13,46 +13,47 @@
 #define PORT_J 9
 #define PORT_K 10
 
-#define ALTF_0  0x0
-#define ALTF_1  0x1
-#define ALTF_2  0x2
-#define ALTF_3  0x3
-#define ALTF_4  0x4
-#define ALTF_5  0x5
-#define ALTF_6  0x6
-#define ALTF_7  0x7
-#define ALTF_8  0x8
-#define ALTF_9  0x9
-#define ALTF_10 0xA
-#define ALTF_11 0xB
-#define ALTF_12 0xC
-#define ALTF_13 0xD
-#define ALTF_14 0xE
-#define ALTF_15 0xF
+#define ALTF_0  0x0UL
+#define ALTF_1  0x1UL
+#define ALTF_2  0x2UL
+#define ALTF_3  0x3UL
+#define ALTF_4  0x4UL
+#define ALTF_5  0x5UL
+#define ALTF_6  0x6UL
+#define ALTF_7  0x7UL
+#define ALTF_8  0x8UL
+#define ALTF_9  0x9UL
+#define ALTF_10 0xAUL
+#define ALTF_11 0xBUL
+#define ALTF_12 0xCUL
+#define ALTF_13 0xDUL
+#define ALTF_14 0xEUL
+#define ALTF_15 0xFUL
+#define ALTF_RS 0xFUL
 
 
-#define MODE_INPUT  (0x0UL)
-#define MODE_OUTPUT (0x1UL)
-#define MODE_ALT_F  (0x2UL)
-#define MODE_ANALOG (0x3UL)
-#define MODE_RESET  (0x3UL)
+#define MODE_INPUT  0x0UL
+#define MODE_OUTPUT 0x1UL
+#define MODE_ALT_F  0x2UL
+#define MODE_ANALOG 0x3UL
+#define MODE_RESET  0x3UL
 
-#define TYPE_PUSH_PULL  (0x0UL)
-#define TYPE_OPEN_DRAIN (0x1UL)
-#define TYPE_RESET      (0x1UL)
-
-
-#define SPEED_LOW   (0x0UL)
-#define SPEED_MED   (0x1UL)
-#define SPEED_HIGH  (0x2UL)
-#define SPEED_MAX   (0x3UL)
-#define SPEED_RESET (0x3UL)
+#define TYPE_PUSH_PULL  0x0UL
+#define TYPE_OPEN_DRAIN 0x1UL
+#define TYPE_RESET      0x1UL
 
 
-#define PULL_NO    (0x0UL)
-#define PULL_UP    (0x1UL)
-#define PULL_DOWN  (0x2UL)
-#define PULL_RESET (0x3UL)
+#define SPEED_LOW   0x0UL
+#define SPEED_MED   0x1UL
+#define SPEED_HIGH  0x2UL
+#define SPEED_MAX   0x3UL
+#define SPEED_RESET 0x3UL
+
+
+#define PULL_NO    0x0UL
+#define PULL_UP    0x1UL
+#define PULL_DOWN  0x2UL
+#define PULL_RESET 0x3UL
 
 void gpio_init (unsigned char port, unsigned char pin, unsigned char mode, unsigned char type, unsigned char speed, unsigned char pull, unsigned char alt_func);
 #endif
